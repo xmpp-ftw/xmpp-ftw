c92f99f49d391dd6422352436f4ca54231c54e6f
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2013-06-08 20:03:33 +0100

    Tests for XEP-0071 complete

--- test/lib/utils/xep-0004.js
+++ test/lib/utils/xep-0004.js
@@ -1,12 +1,7 @@
-require('mocha')
-var dataForm = require('../../lib/utils/xep-0004')
+var dataForm = require('../../../lib/utils/xep-0004')
   , should  = require('should')
   , helper  = require('../../helper')
 
 describe('XEP-0004', function() {
 
-    it('Should do stuff', function(done) {
-        console.log(dataForm)
-        done('Not implemented')
-    })
 })
--- test/lib/utils/xep-0071.js
+++ test/lib/utils/xep-0071.js
@@ -1,17 +1,13 @@
-var xep = require('../../lib/utils/xep-0071')
-require('mocha')
-var should  = require('should')
+var xep0071 = require('../../../lib/utils/xep-0071')
+  , should  = require('should')
   , helper  = require('../../helper')
 
 describe('XEP-0071', function() {
 
-    var caller
-
-    before(function() {
-    })
+    var caller, xmpp
 
     beforeEach(function() {
-        var client = new helper.Eventer()
+        xmpp = new helper.Eventer()
         caller = {
             error: null,
             request: null,
@@ -20,16 +16,75 @@ describe('XEP-0071', function() {
                 this.error = error
                 this.request = request
             },
-            client: client 
+            client: xmpp
         }
     })
 
     it('Exports the XHTML namespace', function() {
-        xep.NS_XHTML.should.equal('http://www.w3.org/1999/xhtml')
+        xep0071.NS_XHTML.should.equal('http://www.w3.org/1999/xhtml')
+    })
+
+    it('Exports the XHTML_IM namespace', function() {
+        xep0071.NS_XHTML_IM.should.equal('http://jabber.org/protocol/xhtml-im')
+    })
+
+    it('Returns error if \'to\' jid not provided', function() {
+        xep0071.builder({}, {}, caller) 
+        caller.error.should.equal('Missing \'to\' key')
+        caller.request.should.be.null
     })
 
     it('Returns error if content property not available', function() {
-         xep.builder({}, {}, caller)
-         caller.error.should.equal('client.error')
+         xep0071.builder({}, { to: 'romeo@example.com' }, caller)
+         caller.error.should.equal('Message content not provided')
+         caller.request.should.be.null
+    })
+
+    it('Can build a plain chat message', function(done) {
+        xmpp.once('stanza', function(stanza) {
+            stanza.is('message').should.be.true
+            stanza.getChild('body').getText().should.equal(request.content)
+            stanza.attrs.type.should.equal('chat')
+            stanza.attrs.to.should.equal(request.to)
+            done()
+        })
+        var request = { content: 'Hello world!', to: 'romeo@example.com' }
+        xep0071.builder(request, { to: request.to }, caller)
+        should.not.exist(caller.error)
+        should.not.exist(caller.request)
+    })
+
+    it('Returns error if unparsable XHTML provided', function() {
+        xep0071.builder(
+            { content: 'Invalid <strong>yes', format: 'xhtml' }, 
+            { to: 'romeo@example.com' }, 
+            caller
+        )
+        caller.error.should.equal('Can not parse XHTML message')
+        caller.request.should.be.null
+    })
+
+    it('Can build an XHTML chat message', function(done) {
+
+        var request = { 
+            content: '<p>XMPP-FTW <b>ROCKS!</b></p>',
+            format: 'xhtml'
+        }
+        var data = { type: 'groupchat', to: 'room@muc.example.com' }
+
+        xmpp.on('stanza', function(stanza) {
+            stanza.is('message').should.be.true
+            stanza.attrs.type.should.equal(data.type)
+            stanza.getChild('body').getText().should.equal('XMPP-FTW ROCKS!')
+            stanza.getChild('html', xep0071.NS_XHTML_IM)
+                .getChild('body', xep0071.NS_XHTML)
+                .children.join()
+                .should.equal(request.content)
+            done()
+        })
+        xep0071.builder(request, data, caller)
+        should.not.exist(caller.error)
+        should.not.exist(caller.request)
+
     })
 })
