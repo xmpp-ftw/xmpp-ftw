46b13d34c41bd0d90b08b51992c71efbfcaee38e
Lloyd Watkin <lloyd@evilprofessor.co.uk>
2014-02-15 14:31:58 +0000

    Building stanza in util file

--- test/lib/utils/xep-0184.js
+++ test/lib/utils/xep-0184.js
@@ -19,9 +19,14 @@ describe('XEP-0184', function() {
         })
 
         it('Adds received element', function() {
-            receipt.build(stanza, { id: '1234' }, 'received')
+            receipt.build(stanza, 'received', { id: '1234' })
             stanza.root().getChild('received', receipt.NS).should.exist
             stanza.root().getChild('received').attrs.id.should.equal('1234')
+        })
+
+        it('Adds \'request\' element by default', function() {
+            receipt.build(stanza)
+            stanza.root().getChild('request', receipt.NS).should.exist
         })
 
     })
